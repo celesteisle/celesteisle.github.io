6b40397ddaa51fd2b3787c8ecdec0a9318cace09
Hugh Carstensen <biohazardincorporated22@gmail.com>
2019-05-11 00:43:16 -0400

    Got dynamic content loader working!

--- templates/dynamic_content.js
+++ templates/dynamic_content.js
@@ -1,38 +1,7 @@
 //Dynamic Page Content Generator
-function dynamicContent(domain){
-    //Get a list of all link elements on the page
-    var links = document.getElementsByClassName("file");
-    //Iterate through each link in the list
-    for(var i = 0, size = links.length; i < size ; i++){
-        var link = links[i];
-        //Get the location of the content file
-        //var filePath = link.href;
-        //store the filepath in the link's ID
-        //link.id = filePath;
-        //Change the link href to redirect user to the content section of the page
-        //link.href = "#content";
-        
-        link.setAttribute("onclick", "clickHandler(self,'" + domain + "')");
-        link.onclick = function(){
-            var filePath = link.href;
-            //location = link.id;
-            console.log(filePath);
-            //Get the file extension of the content file
-            var extension = filePath.substring(filePath.indexOf(".", filePath.indexOf(domain) + domain.length) + 1);
-            //Create content based on file extension
-            if(extension === "png" || extension === "jpg"){
-                imageHandler(filePath);
-            } else if(extension === "xml"){
-                //Call readFile reads xml document and then calls xmlhandler once the httprequest is fulfilled
-                readFile(filePath, domain);
-            }
-            return false;
-        }
-    }
-}
+function clickHandler(filePath, domain){
 
-function clickHandler(clicked, domain){
-    var filePath = clicked.href;
+    //var filePath = clicked.href;
     console.log(filePath);
     //Get the file extension of the content file
     var extension = filePath.substring(filePath.indexOf(".", filePath.indexOf(domain) + domain.length) + 1);
@@ -45,6 +14,41 @@ function clickHandler(clicked, domain){
     }
     return false;
 }
+
+function dynamicContent(domain){
+    //Get a list of all link elements on the page
+    var links = document.getElementsByClassName("file");
+    //Iterate through each link in the list
+    for(var i = 0, size = links.length; i < size ; i++){
+        var link = links[i];
+        //Get the location of the content file
+        //var filePath = link.href;
+        //store the filepath in the link's ID
+        //link.id = filePath;
+        //Change the link href to redirect user to the content section of the page
+        var filePath = link.href;
+        link.href = "#content";
+        link.setAttribute("onclick", "clickHandler('" + filePath + "','" + domain + "')");
+        /*
+        link.onclick = function(){
+            //location = link.id;
+            console.log(filePath);
+            //Get the file extension of the content file
+            var extension = filePath.substring(filePath.indexOf(".", filePath.indexOf(domain) + domain.length) + 1);
+            //Create content based on file extension
+            if(extension === "png" || extension === "jpg"){
+                imageHandler(filePath);
+            }else if(extension === "xml"){
+                //Call readFile reads xml document and then calls xmlhandler once the httprequest is fulfilled
+                readFile(filePath, domain);
+            }
+            return false;
+        }
+        */
+    }
+}
+
+
 
 function imageHandler(imagePath){
     //Get content element
